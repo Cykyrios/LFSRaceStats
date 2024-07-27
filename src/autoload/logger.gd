a7ac84ff27129df225b7e099fc0d9bc8196f457c
Cykyrios <cykyrios@gmail.com>
2024-07-27 07:42:04 +0200

    Log to file instead of printing

--- src/autoload/logger.gd
+++ src/autoload/logger.gd
@@ -1,8 +1,23 @@
 extends Node
 
 
+var file: FileAccess = null
+var file_open := false
+
+
 func _ready() -> void:
-	pass
+	create_log_file()
+
+
+func create_log_file() -> void:
+	file = FileAccess.open("user://%s.log" % [get_date_time_string()], FileAccess.WRITE)
+	if file:
+		file_open = true
+
+
+func close_log_file() -> void:
+	file_open = false
+	file = null
 
 
 func get_date_time_string() -> String:
@@ -10,9 +25,15 @@ func get_date_time_string() -> String:
 
 
 func log_message(message: String) -> void:
-	print("%s - %s" % [get_date_time_string(), message])
+	if not file_open:
+		return
+	file.store_line("%s - %s" % [get_date_time_string(), message])
+	file.flush()
 
 
 func log_packet(packet: InSimPacket) -> void:
+	if not file_open:
+		return
 	var packet_string := packet.to_string()
-	print("%s - %s" % [get_date_time_string(), packet_string])
+	file.store_line("%s - %s" % [get_date_time_string(), packet_string])
+	file.flush()
